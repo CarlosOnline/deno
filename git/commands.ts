c77f8c5163bcfbbc9e35118ad8dddad2c3262bc7
Carlos Gomes <cgomes@iinet.com>
2023-08-29 09:31:38 -0700

    git merge all

--- git/commands.ts
+++ git/commands.ts
@@ -5,10 +5,34 @@ import Utility from "../utility/utility.ts";
 import { Git } from "./index.ts";
 
 export default class GitCommands {
-  @action("git.merge_from_develop,git.mfd", "Merge from develop")
+  @action("git.info", "Get git info")
+  info() {
+    const folder = Options.folder || Deno.cwd();
+    const git = new Git();
+    const info = git.info(folder);
+    logger.info(folder);
+    console.log(info);
+  }
+
+  @action("git.merge", "Merge from develop")
   mergeFromDevelop() {
     const folder = Options.folder || Deno.cwd();
+    GitCommands.mergeFromDevelopBranch(folder);
+  }
 
+  @action("git.mergeAll", "Merge all repos from develop")
+  mergeAllFromDevelop() {
+    const folder = Options.folder || Deno.cwd();
+
+    const git = new Git();
+    const repos = git.listRepos(folder);
+
+    repos.forEach((folder) => {
+      GitCommands.mergeFromDevelopBranch(folder);
+    });
+  }
+
+  private static mergeFromDevelopBranch(folder: string) {
     const git = new Git();
     const info = git.info(folder);
     if (!info) {
@@ -20,6 +44,7 @@ export default class GitCommands {
       Utility.run(Options.git.cmd, "reset --hard".split(" "), folder);
     }
 
-    git.mergeFromBranch(info.develop, folder);
+    git.mergeFromBranch(`origin/${info.develop}`, folder);
+    logger.info(`Merged ${folder}`);
   }
 }
